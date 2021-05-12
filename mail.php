ec4de9f8f044f25815a655a62dd6d37c40360a54
Paras Jain <45164484+Paras-bennett@users.noreply.github.com>
2021-05-12 20:10:32 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+
+if(isset($_POST['name']) && isset($_POST['email']))
+{
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+    $message = $_POST['message'];
+
+    require_once "PHPMailer/PHPMailer.php";
+    require_once "PHPMailer/SMTP.php";
+    require_once "PHPMailer/Exception.php";
+
+    $mail = new PHPMailer();
+
+    //smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "robbyjain10.pj54@gmail.com";
+    $mail->Password = 'ILDBTBTIDLT!!';
+    $mail->Port = 465;
+    $mail->SMTPSecure = "ssl";
+
+    //email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("robbyjain10.pj54@gmail.com");
+    $mail->number = ("$email ($number)");
+    $mail->message = $message;
+
+    if($mail->send()){
+        $status = "success";
+        $response = "Email is sent!";
+    }
+    else
+    {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    } 
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+//redirect
+header("Location: thankyou.html");
+exit();
+?>
